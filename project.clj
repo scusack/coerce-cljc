d97185804944027c56b9cfab38076ab50955202f
Simon Cusack <simon.cusack@gmail.com>
2016-10-05 20:02:14 +1100

    Adds url as required by clojars.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject coerce-cljc "0.0.1-SNAPSHOT"
   :description "A library for coercing simple values from one type to another in Clojure/ClojureScript."
+  :url "https://github.com/scusack/coerce-cljc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure       "1.8.0"]
